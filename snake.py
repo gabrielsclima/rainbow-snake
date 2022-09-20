d15d9d67585edbfb793d5fab5083966bdd3585a2
gabrielsclima <gabrielsclima.2004@gmail.com>
2022-09-20 17:25:13 -0300

    Scroll fixed (improvement needed)
    Conditions were created on line 36 at print() function. If oldSpc variable is not between 0 and 50, it will just print RB[j].
    
    Improvement needed: use only the spc instead of oldSpc. I'm trying to figure out  why the rainbow does not work correctly when you set the spc variable before the print() function (lines 24-25).

--- snake.py
+++ snake.py
@@ -11,27 +11,27 @@ P = "\033[0;35m█" #Purple
 """ Rainbow Management """
 DIRE = -1, 1 #Direction (Left or Right)
 RB = (R, Y, G, C, P) #Rainbow
-spc = 50 #Determines the actual rainbow position inside the loop
+spc = 5 #Determines the actual rainbow position inside the loop
 
 def mod5(x):
     return x % 5
 
 while True:
     f = choice(DIRE)
-
     for i in range(5):
+        oldSpc = spc
         print(" " * spc, end="")
         spc += f
 
         for j in range(5):
-            if spc >= 70:
+            if spc > 50:
                 spc -= 1
 
-            elif spc <= 0:
+            elif spc < 0:
                 spc += 1
 
             am = mod5(f * i+j)
-            print(RB[am], end="")
+            print(RB[am] if oldSpc > 0 and oldSpc < 50 else RB[j], end="")
 
-        print(spc,f)
+        print()
         sleep(0.02)
