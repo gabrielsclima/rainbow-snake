0037592dbd8e84f9060afad11903c0a7f7aca21f
gabrielsclima <gabrielsclima.2004@gmail.com>
2022-09-20 17:40:32 -0300

    Fixed indexError (line 35)
    The error could happen if j was higher than 4.
    - Fixed using mod5(j) (now it makes 'j mod 5', not allowing to pass the index limit of 0-4)

--- snake.py
+++ snake.py
@@ -11,27 +11,28 @@ P = "\033[0;35m█" #Purple
 """ Rainbow Management """
 DIRE = -1, 1 #Direction (Left or Right)
 RB = (R, Y, G, C, P) #Rainbow
-spc = 5 #Determines the actual rainbow position inside the loop
+spc = 50 #Determines the current rainbow position inside the loop
 
 def mod5(x):
     return x % 5
 
 while True:
     f = choice(DIRE)
+    
     for i in range(5):
         oldSpc = spc
         print(" " * spc, end="")
         spc += f
+        
+        for j in range(20): #Rainbow size
 
-        for j in range(5):
-            if spc > 50:
+            if spc > 100: #For now, we have to change this value according to your terminal/prompt command size
                 spc -= 1
 
             elif spc < 0:
                 spc += 1
 
-            am = mod5(f * i+j)
-            print(RB[am] if oldSpc > 0 and oldSpc < 50 else RB[j], end="")
-
+            print(RB[mod5(f * i+j)] if oldSpc > 0 and oldSpc < 50 else RB[mod5(j)], end="")
+        
         print()
         sleep(0.02)
