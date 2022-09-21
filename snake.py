26feaffecc3df66b1985575ab57cc26804c2deaa
unknown <gabrielslcima.2004@gmail.com>
2022-09-21 16:03:03 -0300

    Improvements have been done + rainbow is 100% accurate
    - Created variable END in Colors (line 10);
    - currentPos will be intialized according as the half of POS_MAX (line 17);
    - Removed mod5() (now the calcultion is made directly at j loop (line 25);
    - currentPos will be used as the mod 5 value to avoid an specific issue (line 26);

--- snake.py
+++ snake.py
@@ -7,32 +7,26 @@ Y = "\033[1;33m█" #Yellow
 G = "\033[0;32m█" #Green
 C = "\033[0;36m█" #Cyan
 P = "\033[0;35m█" #Purple
+END = "\033[0m" #End of color
 
 """ Rainbow Management """
-DIRE = -1, 1 #Direction (Left or Right)
+DIRE = -1, 1 # -1 for Left, 1 for Right
 RB = (R, Y, G, C, P) #Rainbow
-spc = 50 #Determines the current rainbow position inside the loop
-
-def mod5(x):
-    return x % 5
+POS_MIN = 0 #Minimum position value
+POS_MAX = int(input("How much the snake can slide on your screen: ")) #Maximum position value
+currentPos = POS_MAX // 2 #Determines the current rainbow position inside the loop (with spaces)
 
 while True:
     f = choice(DIRE)
     
     for i in range(5):
-        oldSpc = spc
-        print(" " * spc, end="")
-        spc += f
+        print(" " * currentPos, end="")
         
         for j in range(20): #Rainbow size
-
-            if spc > 100: #For now, we have to change this value according to your terminal/prompt command size
-                spc -= 1
-
-            elif spc < 0:
-                spc += 1
-
-            print(RB[mod5(f * i+j)] if oldSpc > 0 and oldSpc < 50 else RB[mod5(j)], end="")
+            print(RB[(currentPos + j) % 5], end="")
         
-        print()
+        print(END)
+        currentPos += f
+        if currentPos > POS_MAX or currentPos < POS_MIN:
+            currentPos -= f
         sleep(0.02)
