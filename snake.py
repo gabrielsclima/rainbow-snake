daff62cfc5e566196f4823d6d99ddcf7b481539f
gabrielsclima <gabrielsclima.2004@gmail.com>
2025-01-16 22:43:53 -0300

    Making the code more readable, but with the same old essence.

--- snake.py
+++ snake.py
@@ -1,32 +1,34 @@
-from random import choice
+import random
 from time import sleep
 
-""" Colors """
-R = "\033[0;31m█" #Red
-Y = "\033[1;33m█" #Yellow
-G = "\033[0;32m█" #Green
-C = "\033[0;36m█" #Cyan
-P = "\033[0;35m█" #Purple
-END = "\033[0m" #End of color
+# Colors
+red = "\033[0;31m█"
+yellow = "\033[1;33m█"
+green = "\033[0;32m█"
+cyan = "\033[0;36m█"
+purple = "\033[0;35m█"
+end_color = "\033[0m"
 
-""" Rainbow Management """
-DIRE = -1, 1 # -1 for Left, 1 for Right
-RB = (R, Y, G, C, P) #Rainbow
-POS_MIN = 0 #Minimum position value
-POS_MAX = int(input("How much the snake can slide on your screen: ")) #Maximum position value
-currentPos = POS_MAX // 2 #Determines the current rainbow position inside the loop (with spaces)
+# Rainbow settings
+direction = -1, 1 # -1 for Left, 1 for Right
+color_sequence = (red, yellow, green, cyan, purple)
+minimum_position = 0
+maximum_position = int(input("How much can the snake slide on your screen: "))
+current_position = maximum_position // 2
+rainbow_width = 5
 
+# Start
 while True:
-    f = choice(DIRE)
+    direction_choice = random.choice(direction)
     
-    for i in range(5):
-        print(" " * currentPos, end="")
+    for distance in range(5):
+        print(" " * current_position, end="")
+
+        for width_position in range(rainbow_width):
+            print(color_sequence[(current_position + width_position) % 5], end="")
+        print(end_color)
         
-        for j in range(20): #Rainbow size
-            print(RB[(currentPos + j) % 5], end="")
-        
-        print(END)
-        currentPos += f
-        if currentPos > POS_MAX or currentPos < POS_MIN:
-            currentPos -= f
+        current_position += direction_choice
+        if current_position > maximum_position or current_position < minimum_position:
+            current_position -= direction_choice
         sleep(0.02)
